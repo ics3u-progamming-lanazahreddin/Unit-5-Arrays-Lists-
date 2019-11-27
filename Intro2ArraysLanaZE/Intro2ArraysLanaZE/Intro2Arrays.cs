e0cc41376c1d91aa61c73ee0f6129afcd84f5a59
LanaZahrEddin <43419131+LanaZahrEddin@users.noreply.github.com>
2019-11-27 13:09:35 -0500

    added introduction to arrays

---
+++ Intro2ArraysLanaZE/Intro2ArraysLanaZE/Intro2Arrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intro2ArraysLanaZE
+{
+    static class Intro2Arrays
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmIntro2Arrays());
+        }
+    }
+}
---
+++ Intro2ArraysLanaZE/Intro2ArraysLanaZE/Intro2ArraysForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intro2ArraysLanaZE
+{
+    public partial class frmIntro2Arrays : Form
+    {
+        public frmIntro2Arrays()
+        {
+            InitializeComponent();
+        }
+    }
+}
