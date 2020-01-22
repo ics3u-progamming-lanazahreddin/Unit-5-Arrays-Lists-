e6039724d6c804185f14152c7d2e7f24c539f264
Lana <43419131+LanaZahrEddin@users.noreply.github.com>
2020-01-22 11:00:04 -0500

    added deal card

---
+++ DealCardsLanaZE/DealCardsLanaZE/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace DealCardsLanaZE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picDealercard1 = new System.Windows.Forms.PictureBox();
+            this.picDealercard3 = new System.Windows.Forms.PictureBox();
+            this.picPlayercard2 = new System.Windows.Forms.PictureBox();
+            this.picDealercard2 = new System.Windows.Forms.PictureBox();
+            this.picPlayercard1 = new System.Windows.Forms.PictureBox();
+            this.picPlayercard3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDealercard1
+            // 
+            this.picDealercard1.Image = global::DealCardsLanaZE.Properties.Resources.QS;
+            this.picDealercard1.Location = new System.Drawing.Point(106, 33);
+            this.picDealercard1.Name = "picDealercard1";
+            this.picDealercard1.Size = new System.Drawing.Size(99, 142);
+            this.picDealercard1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDealercard1.TabIndex = 0;
+            this.picDealercard1.TabStop = false;
+            this.picDealercard1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // picDealercard3
+            // 
+            this.picDealercard3.Image = global::DealCardsLanaZE.Properties.Resources.QS;
+            this.picDealercard3.Location = new System.Drawing.Point(584, 33);
+            this.picDealercard3.Name = "picDealercard3";
+            this.picDealercard3.Size = new System.Drawing.Size(99, 142);
+            this.picDealercard3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDealercard3.TabIndex = 1;
+            this.picDealercard3.TabStop = false;
+            // 
+            // picPlayercard2
+            // 
+            this.picPlayercard2.Image = global::DealCardsLanaZE.Properties.Resources.QS;
+            this.picPlayercard2.Location = new System.Drawing.Point(351, 226);
+            this.picPlayercard2.Name = "picPlayercard2";
+            this.picPlayercard2.Size = new System.Drawing.Size(99, 142);
+            this.picPlayercard2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlayercard2.TabIndex = 2;
+            this.picPlayercard2.TabStop = false;
+            // 
+            // picDealercard2
+            // 
+            this.picDealercard2.Image = global::DealCardsLanaZE.Properties.Resources.QS;
+            this.picDealercard2.Location = new System.Drawing.Point(351, 33);
+            this.picDealercard2.Name = "picDealercard2";
+            this.picDealercard2.Size = new System.Drawing.Size(99, 142);
+            this.picDealercard2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDealercard2.TabIndex = 3;
+            this.picDealercard2.TabStop = false;
+            // 
+            // picPlayercard1
+            // 
+            this.picPlayercard1.Image = global::DealCardsLanaZE.Properties.Resources.QS;
+            this.picPlayercard1.Location = new System.Drawing.Point(106, 226);
+            this.picPlayercard1.Name = "picPlayercard1";
+            this.picPlayercard1.Size = new System.Drawing.Size(99, 142);
+            this.picPlayercard1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlayercard1.TabIndex = 4;
+            this.picPlayercard1.TabStop = false;
+            // 
+            // picPlayercard3
+            // 
+            this.picPlayercard3.Image = global::DealCardsLanaZE.Properties.Resources.QS;
+            this.picPlayercard3.Location = new System.Drawing.Point(584, 235);
+            this.picPlayercard3.Name = "picPlayercard3";
+            this.picPlayercard3.Size = new System.Drawing.Size(99, 142);
+            this.picPlayercard3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlayercard3.TabIndex = 5;
+            this.picPlayercard3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picPlayercard3);
+            this.Controls.Add(this.picPlayercard1);
+            this.Controls.Add(this.picDealercard2);
+            this.Controls.Add(this.picPlayercard2);
+            this.Controls.Add(this.picDealercard3);
+            this.Controls.Add(this.picDealercard1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDealercard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayercard3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDealercard1;
+        private System.Windows.Forms.PictureBox picDealercard3;
+        private System.Windows.Forms.PictureBox picPlayercard2;
+        private System.Windows.Forms.PictureBox picDealercard2;
+        private System.Windows.Forms.PictureBox picPlayercard1;
+        private System.Windows.Forms.PictureBox picPlayercard3;
+    }
+}
+
---
+++ DealCardsLanaZE/DealCardsLanaZE/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DealCardsLanaZE
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        List<Image> ListCardImages = new List<Image>();
+        List<int> ListCardValues = new List<int>();
+        Random randnum = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateDeck();
+        }
+        //procedure: CreateDeck
+        //input: void
+        //output: void
+        //Description:  this procedure populates the deck of cards and the values
+        private void CreateDeck()
+        {
+            ListCardImages.Clear();
+            ListCardValues.Clear();
+            //Add all cards to a list
+            ListCardImages.Add(Properties.Resources.AC);
+            ListCardImages.Add(Properties.Resources.AD);
+            ListCardImages.Add(Properties.Resources.AH);
+            ListCardImages.Add(Properties.Resources.AS);
+
+            ListCardImages.Add(Properties.Resources._2C);
+            ListCardImages.Add(Properties.Resources._2D);
+            ListCardImages.Add(Properties.Resources._2H);
+            ListCardImages.Add(Properties.Resources._2S);
+
+            ListCardImages.Add(Properties.Resources._3C);
+            ListCardImages.Add(Properties.Resources._3D);
+            ListCardImages.Add(Properties.Resources._3H);
+            ListCardImages.Add(Properties.Resources._3S);
+
+            ListCardImages.Add(Properties.Resources._4C);
+            ListCardImages.Add(Properties.Resources._4D);
+            ListCardImages.Add(Properties.Resources._4H);
+            ListCardImages.Add(Properties.Resources._4S);
+
+            ListCardImages.Add(Properties.Resources._6C);
+            ListCardImages.Add(Properties.Resources._6D);
+            ListCardImages.Add(Properties.Resources._6H);
+            ListCardImages.Add(Properties.Resources._6S);
+
+            ListCardImages.Add(Properties.Resources._7C);
+            ListCardImages.Add(Properties.Resources._7D);
+            ListCardImages.Add(Properties.Resources._7H);
+            ListCardImages.Add(Properties.Resources._7S);
+
+            ListCardImages.Add(Properties.Resources._8C);
+            ListCardImages.Add(Properties.Resources._8D);
+            ListCardImages.Add(Properties.Resources._8H);
+            ListCardImages.Add(Properties.Resources._8S);
+
+            ListCardImages.Add(Properties.Resources._10C);
+            ListCardImages.Add(Properties.Resources._10D);
+            ListCardImages.Add(Properties.Resources._10H);
+            ListCardImages.Add(Properties.Resources._10S);
+
+            ListCardImages.Add(Properties.Resources.JC);
+            ListCardImages.Add(Properties.Resources.JD);
+            ListCardImages.Add(Properties.Resources.JH);
+
+            ListCardImages.Add(Properties.Resources.QC);
+            ListCardImages.Add(Properties.Resources.QD);
+            ListCardImages.Add(Properties.Resources.QH);
+            ListCardImages.Add(Properties.Resources.QS);
+
+
+            //Add the values of the cards respectively to the list of cards
+            ListCardValues.Add(1);
+            ListCardValues.Add(1);
+            ListCardValues.Add(1);
+            ListCardValues.Add(1);
+
+            ListCardValues.Add(2);
+            ListCardValues.Add(2);
+            ListCardValues.Add(2);
+            ListCardValues.Add(2);
+
+            ListCardValues.Add(3);
+            ListCardValues.Add(3);
+            ListCardValues.Add(3);
+            ListCardValues.Add(3);
+
+            ListCardValues.Add(4);
+            ListCardValues.Add(4);
+            ListCardValues.Add(4);
+            ListCardValues.Add(4);
+
+            ListCardValues.Add(5);
+            ListCardValues.Add(5);
+            ListCardValues.Add(5);
+            ListCardValues.Add(5);
+
+            ListCardValues.Add(6);
+            ListCardValues.Add(6);
+            ListCardValues.Add(6);
+            ListCardValues.Add(6);
+
+            ListCardValues.Add(7);
+            ListCardValues.Add(7);
+            ListCardValues.Add(7);
+            ListCardValues.Add(7);
+
+            ListCardValues.Add(8);
+            ListCardValues.Add(8);
+            ListCardValues.Add(8);
+            ListCardValues.Add(8);
+
+            ListCardValues.Add(9);
+            ListCardValues.Add(9);
+            ListCardValues.Add(9);
+            ListCardValues.Add(9);
+
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+            ListCardValues.Add(10);
+
+
+        }
+        //procedure: DealCard
+        //input: ref picturebox aPictureBox, int randomIndex
+        //output: int
+        //Description:  this function deals the cards to each picture box
+
+        private int DealCard(ref PictureBox aPictureBox, int randomIndex)
+        {
+            Image Card;
+            int Value;
+
+            //get the image from the random index
+            Card = ListCardImages[randomIndex];
+
+            //put the card image in the picturebox passed by reference
+            aPictureBox.Image = Card;
+
+            //remove the image from the list of cards
+            ListCardImages.RemoveAt(randomIndex);
+            Console.WriteLine("***List of card images count = " + ListCardImages.Count());
+
+            //get the value of the card
+            Value = ListCardValues[randomIndex];
+            ListCardValues.RemoveAt(randomIndex);
+            Console.WriteLine("***List of card values count = " + ListCardValues.Count());
+            return Value;
+
+        }
+
+        private void picDealercard3_Click(object sender, EventArgs e)
+        {
+            if (ListCardImages.Count() == 0)
+            {
+                CreateDeck();
+            }
+            else
+            {
+                int random = randnum.Next(0, ListCardImages.Count() - 1);
+                DealCard(ref this.picDealercard3, random);
+            }
+
+
+        }
+
+        private void picDealercard1_Click(object sender, EventArgs e)
+        {
+            if (ListCardImages.Count() == 0)
+            {
+                CreateDeck();
+            }
+            else
+            {
+                int random = randnum.Next(0, ListCardImages.Count() - 1);
+                DealCard(ref this.picDealercard1, random);
+            }
+        }
+
+
+
+        private void picPlayercard3_Click(object sender, EventArgs e)
+        {
+            if (ListCardImages.Count() == 0)
+            {
+                CreateDeck();
+            }
+            else
+            {
+                int random = randnum.Next(0, ListCardImages.Count() - 1);
+                DealCard(ref this.picPlayercard3, random);
+            }
+        }
+
+        private void picDealercard2_Click(object sender, EventArgs e)
+        {
+            if (ListCardImages.Count() == 0)
+            {
+                CreateDeck();
+            }
+            else
+            {
+                int random = randnum.Next(0, ListCardImages.Count() - 1);
+                DealCard(ref this.picDealercard2, random);
+            }
+        }
+
+
+
+        private void picPlayercard2_Click(object sender, EventArgs e)
+        {
+            if (ListCardImages.Count() == 0)
+            {
+                CreateDeck();
+            }
+            else
+            {
+                int random = randnum.Next(0, ListCardImages.Count() - 1);
+                DealCard(ref this.picPlayercard2, random);
+            }
+        }
+
+        private void picPlayercard1_Click_1(object sender, EventArgs e)
+        {
+            if (ListCardImages.Count() == 0)
+            {
+                CreateDeck();
+                Console.WriteLine("***Created new deck.");
+            }
+            else
+            {
+                int random = randnum.Next(0, ListCardImages.Count() - 1);
+                DealCard(ref this.picPlayercard1, random);
+            }
+        }
+    }
+}
